49bdb52a4f9fd74a36f3d36c879a9e01f4760b12
mlxxx63 <105572717+mlxxx63@users.noreply.github.com>
2023-06-06 23:42:26 +0700

    Update App.java

--- Project CP with Jenna/App.java
+++ Project CP with Jenna/App.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
-public class App 
+// main fuction to run the code
+public class app 
 {
+    // An ArrayList that hold the playList ArrayList
     public static ArrayList<PlayList> albumn = new ArrayList<>();
 
     public static void main(String[] args) 
     {
         Scanner scan = new Scanner(System.in);
-
+        // Create a Scanner object
+        
+    // In this while loop, the user can make choices of what they want to do 
         while(true)
         {
             showMenu();
@@ -64,13 +68,14 @@ public class App
             // It should only be closed once, and that should be at the end of the main method.
             scan.close();
     }
-
+    
+     // show the text base of what this program can do
     public static void showMenu() 
     {
         System.out.println("");
         System.out.println("Main Menu: ");
         System.out.println("-------------------------------------------------");
-        System.out.println("|   Option 1: Create album and playlists         |");
+        System.out.println("|   Option 1: Create playlists                   |");
         System.out.println("|   Option 2: Show playlists                     |");
         System.out.println("|   Option 3: Add new songs to a playlist        |");
         System.out.println("|   Option 4: Remove songs from a playlist       |");
@@ -89,15 +94,23 @@ public class App
             int numberPlaylists = scan.nextInt();
             scan.nextLine();  // TB says: This will consume the leftover newline character.
             System.out.println(" ");
-
+            
+             // enter the information of the playlist 
             for (int i = 0; i < numberPlaylists; i++) {
                 System.out.println("Enter the info for Playlist " + (i + 1));
+                 // add the playList ArrrayList to the albumn ArrayList 
                 albumn.add(new PlayList());
+               /* call out the setName method for the user to enter infos,
+               *and then it gets added to playList then albumn
+               */
                 albumn.get(i).setName(scan);
+                 /* call out the setGenre method for the user to enter infos,
+                 *and then it gets added to playList then albumn
+                 */
                 albumn.get(i).setGenre(scan);
                 System.out.println(" ");
             }
-        
+        // Print out the infos that was just inputed 
         System.out.println("Playlists created:");
         for(int i = 0; i < albumn.size(); i++) 
         {
@@ -105,12 +118,14 @@ public class App
         }
         
     }
-
+    // Print out the albumn (prints put the playlist information)
     public static void showPlaylists() 
     {
         for (int i = 0; i < albumn.size(); i++) {
-            PlayList playlist = albumn.get(i);  // initialize a varible playlist type PlayList = to albumn.get(i)
-            ArrayList<Song> songs = playlist.getSongList();  // initialize a varible songs = to play playlist.getSongList()
+             // initialize a varible playlist type PlayList = to albumn.get(i)
+            PlayList playlist = albumn.get(i); 
+            // initialize a varible songs = to playlist.getSongList()
+            ArrayList<Song> songs = playlist.getSongList(); 
 
             System.out.println("This is playlist " + (i + 1));
             System.out.println("Name: " + playlist.getName());
@@ -131,7 +146,7 @@ public class App
             System.out.println();
         }
     }
-
+     // add songs to the playList Array
     public static void addSongsToPlaylist(Scanner scan) 
     {
         System.out.println("Which playlist do you want to add new song(s) to?");
@@ -145,13 +160,17 @@ public class App
         for (int i = 0; i < numSongs; i++) 
         {
             System.out.println("Enter the details for song " + (i + 1));
+             /*its (choice -1) because of the index in ArrayList, 
+             *numSongs is added to the method addSong
+             *because we only want to make a certain amount of songs 
+             */
             albumn.get(choice - 1).addSong(numSongs, scan);
             System.out.println(" ");
         }
         
         
     }
-
+    //the purpose is to save the ArrayList albumn to a text file 
     public static void savePlaylistsToFile() {
         try {
             FileWriter fileWriter = new FileWriter("savefile.txt");
@@ -176,24 +195,37 @@ public class App
             e.printStackTrace();
         }
     }
-
+    
+    // allow the user to change the infos of the PlayList 
     public static void editPlaylistDetails(Scanner scan) {
         System.out.println("Enter the playlist index you want to edit: ");
         int playlistIndex = scan.nextInt();
         scan.nextLine();
 
+        /* if and else logic serves the purpose of making sure
+        *the user enter the right input*/
+        
         if (playlistIndex >= 1 && playlistIndex <= albumn.size()) 
         {
+             // initialize a varible playlist type PlayList = 
+             //to albumn.get(playlistIndex - 1)
             PlayList playlist = albumn.get(playlistIndex - 1);
             System.out.println("Current details for Playlist " + playlistIndex);
             System.out.println("Name: " + playlist.getName());
             System.out.println("Genre: " + playlist.getGenre());
 
             System.out.println("Enter the updated name (press Enter to skip): ");
-            playlist.setName(scan);
-
+            String name = scan.nextLine();
+            if(name.isEmpty()==false)
+            {
+                playlist.setNewName(name);
+            }
+            
             System.out.println("Enter the updated genre (press Enter to skip): ");
-            playlist.setGenre(scan);
+            String genre = scan.nextLine();
+            if(genre.isEmpty()==false){
+                playlist.setNewGenre(genre);
+            }
 
             System.out.println("Playlist details updated successfully!");
         } 
@@ -203,6 +235,7 @@ public class App
         }
     }
 
+    // remove song from a specific playlist
     public static void removeSongsFromPlaylist(Scanner scan) {
 
         System.out.println("Enter the playlist index to remove songs from: ");
@@ -213,7 +246,8 @@ public class App
         {
             PlayList playlist = albumn.get(playlistIndex - 1);
             ArrayList<Song> songs = playlist.getSongList();
-
+             
+             // if the Array is empty then nothing happen
             if (songs.isEmpty()) 
             {
                 System.out.println("No songs in the playlist to remove!");
@@ -229,7 +263,7 @@ public class App
                 System.out.println("Enter the index of the song to remove: ");
                 int songIndex = scan.nextInt();
                
-
+                 // this codes resulted in deleting a chosen song 
                 if (songIndex >= 1 && songIndex <= songs.size()) 
                 {
                     playlist.removeSong(songIndex - 1);
@@ -271,6 +305,7 @@ public class App
     
                 if (confirmation.equalsIgnoreCase("yes")) 
                 {
+                    // clear function resulted in every elements in the array getting cleared 
                     playlist.clear();
                     System.out.println("Playlist cleared successfully!");
                 } 
@@ -286,5 +321,3 @@ public class App
         }
     }
 }
-    
-
